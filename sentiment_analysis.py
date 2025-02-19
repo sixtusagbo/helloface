2446942f555126974cc4a4702f1c5c214a6b8b0c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-19 16:53:56 +0100

    Use tokenization

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,28 @@
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 classifier = pipeline("sentiment-analysis")
 
 result = classifier("I'm very happy to use the Transformers library")
 
 print(result)
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+result = classifier("I'm very happy to use the Transformers library")
+
+print(result)
+
+sequence = "Using a Transformer network is simple"
+result = tokenizer(sequence)
+print(result)
+tokens = tokenizer.tokenize(sequence)
+print(tokens)
+ids = tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+decoded_string = tokenizer.decode(ids)
+print(decoded_string)

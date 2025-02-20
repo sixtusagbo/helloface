834027d468098d8562339945edbf429655f763ed
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-20 17:55:11 +0100

    Play with NER in Igbo

---
+++ igbo_ner.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+model_name = "mbeukman/xlm-roberta-base-finetuned-igbo-finetuned-ner-igbo"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
+
+nlp = pipeline(
+    "ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple"
+)
+example = "Chinua Achebe bụ onye edemede Nigeria"
+
+ner_results = nlp(example)
+print(ner_results)
